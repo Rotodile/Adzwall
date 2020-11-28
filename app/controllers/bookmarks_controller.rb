7ed7465eb197ddd35b76ad55f3167189fda67e02
root <areeba.aic@gmail.com>
2020-11-28 16:10:28 +0500

    fixed bookmarks error

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,10 +3,10 @@ class BookmarksController < ApplicationController
         bookmark = Bookmark.where(ad: Ad.find(params[:ad]), user: current_user)
         if bookmark == []
             Bookmark.create(ad: Ad.find(params[:ad]), user: current_user)
-            @bookmark_exists == true
+            @bookmark_exists = true
         else
             bookmark.destroy_all
-            @bookmark_exists == false
+            @bookmark_exists = false
         end
         respond_to do |format|
             format.html {}
