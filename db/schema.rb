47393de8f7343026ed34be7cfe95388c1be04a2d
root <areeba.aic@gmail.com>
2020-11-29 21:22:19 +0500

    fixed error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_25_111655) do
+ActiveRecord::Schema.define(version: 2020_11_28_123625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
